938bc8767ecdcaa987a098a07aa2be7e8fad06ad
jakub <jacob_the_liar@tlen.pl>
2025-04-17 09:31:06 +0200

    feature(backend): initialize the backend project using springinitializr.io

---
+++ source/backend/src/test/java/pl/jit/flashcards/FlashcardsApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.jit.flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// check context loads
+	}
+
+}
